2e8f2baa0f304bb33d8ecaff71ad64b0e88c8315
Justin <jgwitkowski@gmail.com>
2023-11-17 13:52:23 -0500

    change back vit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,14 @@
-// import react from '@vitejs/plugin-react-swc'
-// import { defineConfig } from 'vite'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   build: {
-//     sourcemap: true,
-//   },
-//   plugins: [react()],
-//   server: {
-//     host: true,
-//     port: 3000,
-//   },
-// })
-
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/ccr-ui',
-// })
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   sourcemap: true,
-  // },
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()],
-  base: '/ccr-ui/',
-  // server: {
-  //   host: true,
-  //   port: 3000,
-  // },
+  server: {
+    host: true,
+    port: 3000,
+  },
 })
