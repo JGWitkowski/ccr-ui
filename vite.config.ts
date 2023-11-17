f17b556033d75d9e4cf583291dedbfe221fd8817
Justin <jgwitkowski@gmail.com>
2023-11-17 14:18:47 -0500

    try gh

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import react from '@vitejs/plugin-react-swc'
+// import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   build: {
+//     sourcemap: true,
+//   },
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//     port: 3000,
+//   },
+// })
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
+  base: '/ccr-ui/',
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
 })
